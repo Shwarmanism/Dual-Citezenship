7fe22c6382fa7b0dcaa6da30c4a45b7ac6a54861
Shwarmanism <161039124+Shwarmanism@users.noreply.github.com>
2025-05-19 23:17:37 +0800

    Backend

--- backend/login.py
+++ backend/login.py
@@ -1,2 +1,22 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_wtf.csrf import CSRFProtect
+from flask_bcrypt import Bcrypt
+from flask_login import UserMixin
+import os
+
+app = Flask(__name__, template_folder='../templates')
+bycrypt = Bcrypt(app)
+SECRET_KEY = os.urandom(32)
+app.config['SERCRET_KEY'] = SECRET_KEY
+csrf = CSRFProtect(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+
+db = SQLAlchemy(app)
+
+class User(db.Model, UserMixin):
+    id_no = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String[35], unique=True, nullable=False)
+    email = db.Column(db.String[50], unique=True, nullable=False)
+    password = db.Column(db.String[35], unique=True, nullable=False)
+

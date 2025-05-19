8c0ab073492eff8e1a451c9855013ab22bc34fbc
Shwarmanism <161039124+Shwarmanism@users.noreply.github.com>
2025-05-19 23:46:34 +0800

    Update login.py

--- backend/login.py
+++ backend/login.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf.csrf import CSRFProtect
 from flask_bcrypt import Bcrypt
-from flask_login import UserMixin
+from flask_login import UserMixin, current_user, login_manager, LoginManager
 import os
 
 app = Flask(__name__, template_folder='../templates')
@@ -10,6 +10,16 @@ bycrypt = Bcrypt(app)
 SECRET_KEY = os.urandom(32)
 app.config['SERCRET_KEY'] = SECRET_KEY
 csrf = CSRFProtect(app)
+csrf.init_app(app)
+
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
+@login_manager.user_loder
+
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 
 db = SQLAlchemy(app)
